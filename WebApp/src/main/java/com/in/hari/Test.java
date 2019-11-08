06b96ec0ac4710717120aed4d56a5017d9143fc2
hari prasa reddy <hari.09531@gmail.com>
2019-11-09 00:28:33 +0530

    STS

--- WebApp/src/main/java/com/in/hari/Test.java
+++ WebApp/src/main/java/com/in/hari/Test.java
@@ -10,7 +10,8 @@ public class Test {
 		System.out.println("line4..");
 		System.out.println("line5..");
 		System.out.println("line6..");
-		System.out.println("line7..STS23");
+		
+		System.out.println("line7..STS");
 	
 
 		
