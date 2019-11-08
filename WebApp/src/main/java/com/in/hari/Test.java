742e101107f874dcae4ae791e2dda3c59bc171d5
hari prasa reddy <hari.09531@gmail.com>
2019-11-09 00:03:42 +0530

    11

--- WebApp/src/main/java/com/in/hari/Test.java
+++ WebApp/src/main/java/com/in/hari/Test.java
@@ -10,7 +10,7 @@ public class Test {
 		System.out.println("line4..");
 		System.out.println("line5..");
 		System.out.println("line6..");
-		System.out.println("line7..STS");
+		System.out.println("line7..STS1");
 
 		
 	}
